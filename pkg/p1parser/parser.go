40927696f0b4efe6ab75e8d97fe9a66c30c82918
toby lorne <toby@toby.codes>
2024-08-03 14:19:25 +0200

    verwijder all null bytes
    niet alleen voor en achter
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- pkg/p1parser/parser.go
+++ pkg/p1parser/parser.go
@@ -44,7 +44,7 @@ func Parse(b []byte) (*EnergieTelegram, error) {
 
 	for s.Scan() {
 		line := s.Bytes()
-		line = bytes.Trim(line, "\x00")
+		line = bytes.ReplaceAll(line, []byte("\x00"), []byte(""))
 
 		if len(line) < 10 {
 			continue
